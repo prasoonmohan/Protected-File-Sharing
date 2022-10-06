44a7eb999cde8096117239d661b13dbab78b64d8
Prasoon <thisismeprasu@gmail.com>
2022-10-06 13:59:20 +0530

    file protected

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const File = require('./models/file')
 
 const express = require("express")
 const app = express()
-var port = 3000
+
+app.use(express.urlencoded({ extended : true }))
 
 const upload = multer({ dest : "uploads" })
 
@@ -36,14 +37,30 @@ app.post("/upload",upload.single("file"),async (req,res)=>{
    res.render("index", { fileLink : `${req.headers.origin}/file/${file.id}`})
 })
 
-app.get("/file/:id",async (req,res)=>{
+app.route("/file/:id").get(handleDownload).post(handleDownload)
+
+// app.get("/file/:id",handleDownload)
+// app.post("/file/:id",handleDownload)
+
+async function handleDownload(req,res){
     const file = await File.findById(req.params.id)
+
+    if(file.password != null){
+        if(req.body.password == null){
+            res.render("password")
+            return
+        }
+        if(!(await bcrypt.compare(req.body.password , file.password))) {
+            res.render("password" , { error : true })
+            return
+        }
+    }
 
     file.downloadCount++
     await file.save()
     console.log(file.downloadCount);
 
     res.download(file.path , file.originalName)
-})
+}
 
 app.listen(process.env.PORT)
